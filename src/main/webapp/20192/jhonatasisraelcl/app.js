0ddd94767686d795674ac26841e3934369aaf135
Jhonatas <costajhonatas9@gmail.com>
2019-08-19 02:03:01 -0300

    Update app.js

--- src/main/webapp/20192/jhonatasisraelcl/app.js
+++ src/main/webapp/20192/jhonatasisraelcl/app.js
@@ -12,3 +12,16 @@ t1.fromTo(hero, 1, {height:"0%"}, {height:'80%',  ease: Power2.easeInOut})
 .fromTo(logo, 0.5, {opacity: 0, x: 30}, {opacity:1,  x:0}, "-=0.5")
 .fromTo(hamburger, 0.5, {opacity: 0, x: 30}, {opacity:1,  x:0}, "-=0.5")
 .fromTo(headline, 0.5, {opacity: 0, x: 30}, {opacity:1,  x:0}, "-=0.5");
+
+//---------------------------------------------------------------
+var controle = 0;
+function mudarcss() { 
+    if(controle ==0){
+    	document.getElementById("demo").style.background = "blue";
+        controle++;
+        }
+    else{
+    	document.getElementById("demo").style.background = "green";
+        controle--;
+        }
+}
